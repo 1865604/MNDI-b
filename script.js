a3770c53f1392672baa71716d1cde3f42acc7e9c
1865604 <tsukasaokumurathomas@gmail.com>
2026-06-03 14:15:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,29 @@ document.getElementById("shelters").textContent =
 
 }
 
-loadData();
+async function loadData(){
+
+  await loadWeather();
+  await loadAlerts();
+
+};
 
 setInterval(loadData,300000);
+async function loadAlerts(){
+
+  try{
+
+    const res = await fetch("/api/alerts");
+    const data = await res.json();
+
+    document.getElementById("alerts").innerHTML =
+      "警報情報を取得しました";
+
+  }catch{
+
+    document.getElementById("alerts").textContent =
+      "警報情報取得失敗";
+
+  }
+
+}
